5d1c054d74cbe0909c89a64280bdf903fcd9ae06
Harsh <harshsindhupvt@gmail.com>
2025-04-30 20:48:24 +0530

    updates

--- ChannelService/services/email.service.js
+++ ChannelService/services/email.service.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer'
 export const sendEmail = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      host: process.env.MAIL_HOST, 
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
